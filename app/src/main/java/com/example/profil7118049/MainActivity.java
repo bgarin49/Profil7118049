6f3fe75935f3cba93169de9651aad8c6a8bd1d9a
bgarin49 <56551934+bgarin49@users.noreply.github.com>
2019-12-28 11:28:39 +0700

    Tambah setting bahasa indonesia

--- app/src/main/java/com/example/profil7118049/MainActivity.java
+++ app/src/main/java/com/example/profil7118049/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.profil7118049;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btn_setting;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_setting=(Button)findViewById(R.id.btn_setting);
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+
+            }
+
+        });
     }
-}
+    }
